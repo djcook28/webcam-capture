fd0dc6fe3267478ddac3cd52d4f7a4961586e5f4
dcook <daniel.j.cook.28@gmail.com>
2025-06-08 08:51:46 -0500

    Added frame modifications to blur and grayscale to improve performance.  Also added a frame that highlights differences between current and last frame

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import cv2
 import time
 
 video = cv2.VideoCapture(0)
+last_frame = None
 
 while True:
-    check, frame = video.read()
-    cv2.imshow("My Video", frame)
+    check, current_frame = video.read()
+    current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+    current_frame = cv2.GaussianBlur(current_frame, (21, 21), 0)
+
+    if last_frame is None:
+        last_frame = current_frame
+
     time.sleep(1)
 
     key = cv2.waitKey(1)
 
+    delta_frame = cv2.absdiff(last_frame, current_frame)
+    cv2.imshow("My Video", delta_frame)
+
     if key == ord("q"):
         break
 
+    last_frame = current_frame
 video.release()
