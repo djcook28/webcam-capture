a5040fc93a3f8a689cc17b90ead329d324b66bc6
dcook <daniel.j.cook.28@gmail.com>
2025-06-14 11:35:11 -0500

    added emailing functionality

--- send_email.py
+++ send_email.py
@@ -1,3 +1,34 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+import imghdr
 
-def send_email(image):
-    print('image')
+PASSWORD = os.environ.get("pythonGmailPass")
+host = "smtp.gmail.com"
+port = 587
+
+load_dotenv()
+SENDER = os.getenv("SENDER")
+
+RECEIVER = SENDER
+def send_email(image_path):
+    email_message = format_email(image_path)
+
+    gmail = smtplib.SMTP(host, port)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+def format_email(image_path):
+    message = EmailMessage()
+    message["Subject"] = "New movement detected"
+    message.set_content("New distinct movement detected")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+    return message
